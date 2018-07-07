643142b4a054fcb7ee6ee5f47a068cfe339a6101
MingYo <melvin860217@gmail.com>
2018-07-07 11:29:07 +0800

    利用Github做第一次push

--- app/src/main/java/com/example/mingyo/githubtest/MainActivity.java
+++ app/src/main/java/com/example/mingyo/githubtest/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.mingyo.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    void testPush() {
+        Log.d("test","測試用push上傳程式");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
